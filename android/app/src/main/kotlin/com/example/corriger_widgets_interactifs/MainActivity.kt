f5881378d6a3badb95013c5e28034d90ebca79c2
yanndev <yannicknici@gmail.com>
2023-06-19 10:49:37 +0000

    premier commit

---
+++ android/app/src/main/kotlin/com/example/corriger_widgets_interactifs/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.corriger_widgets_interactifs
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/interactifs_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class InteractifsPage extends StatefulWidget {
+  const InteractifsPage({super.key});
+
+
+  @override
+  State<InteractifsPage> createState() => InteractifsPageState();
+}
+
+class InteractifsPageState extends State<InteractifsPage> {
+  @override
+  Widget build(BuildContext context) {
+   return Scaffold(
+     appBar: AppBar(
+       backgroundColor: Colors.white60,
+       title: const Text('State Managemant', style: TextStyle(color: Colors.brown, fontFamily: CupertinoIcons.iconFont),
+       ),
+       actions: const [
+         Padding(padding: EdgeInsets.only(right: 20),
+         child: Icon(Icons.account_circle, color: Colors.brown,),
+         )
+       ],
+     ),
+     body:  SingleChildScrollView(
+       child: Container(),
+     ),
+   );
+  }
+
+}
