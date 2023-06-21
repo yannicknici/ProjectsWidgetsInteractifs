0f3544ed06fc0bfc3873213906331d886226ac04
yanndev <yannicknici@gmail.com>
2023-06-21 17:15:29 +0000

    troisieme commit

--- lib/interactifs_page.dart
+++ lib/interactifs_page.dart
@@ -1,3 +1,4 @@
+import 'package:corriger_widgets_interactifs/profile.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -10,24 +11,102 @@ class InteractifsPage extends StatefulWidget {
 }
 
 class InteractifsPageState extends State<InteractifsPage> {
-  @override
 
+   Profile myProfile  = Profile( surname: 'yannick', name: 'yao');
+   late TextEditingController surname;
+   late TextEditingController name;
+   late TextEditingController secret;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    surname =TextEditingController();
+    name =TextEditingController();
+    secret =TextEditingController();
+    surname.text = myProfile.surname;
+    name.text = myProfile.name;
+    secret.text = myProfile.secret;
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    surname.dispose();
+    name.dispose();
+    secret.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
    return Scaffold(
      appBar: AppBar(
-       backgroundColor: Colors.white60,
-       title: const Text('State Managemant', style: TextStyle(color: Colors.brown, fontFamily: CupertinoIcons.iconFont),
+       backgroundColor: Colors.purple,
+       title: const Text('Mon profile', style: TextStyle(color: Colors.white, fontFamily: CupertinoIcons.iconFont),
        ),
-       actions: const [
-         Padding(padding: EdgeInsets.only(right: 20),
-         child: Icon(Icons.account_circle, color: Colors.brown,),
-         )
-       ],
      ),
      body:  SingleChildScrollView(
-       child: Container(),
+       child: Column(
+         mainAxisSize: MainAxisSize.max,
+         children: [
+           Card(
+             margin: const EdgeInsets.only(left: 2, top: 0, right: 2, bottom: 10),
+             elevation: 10,
+             color: Colors.purpleAccent.shade100,
+             child: Container(
+               width:  MediaQuery.of(context).size.width,
+               margin: const EdgeInsets.all(10),
+               padding: const EdgeInsets.all(10),
+               child: Column(
+                 children: [
+                   Text(myProfile.setName()),
+                   Text( 'Age : ${myProfile.setAge()}'),
+                   Text('Taille : ${myProfile.setHeight()}'),
+                   Text('Genre : ${myProfile.genderString()}'),
+                   Text('Hobbies : ${myProfile.setHobbies()}'),
+                   Text('Langage de programmation favori : ${myProfile.favoriteLang}')
+                 ],
+               ),
+             ),
+           ),
+           const Divider(color: Colors.purpleAccent, thickness: 2,),
+           myText(text: 'Modifier les infos', couleur: Colors.red, weight: FontWeight.bold ),
+           myTextField(controller: surname, hint: 'entrez votre prenom'),
+           myTextField(controller: name, hint: 'entrez votre nom'),
+           myTextField(controller: secret, hint: 'donnez nous un secret', isSecret: true)
+         ],
+       ),
      ),
    );
+  }
+
+  Text myText({required String text, required Color couleur, FontWeight weight = FontWeight.normal}) => Text(text, style: TextStyle(fontSize: 18, color: couleur),);
+
+  TextField myTextField({ required TextEditingController controller, required String hint, bool isSecret = false}){
+    return TextField(
+      controller: controller,
+      decoration: InputDecoration(
+        hintText: hint,
+
+      ),
+      obscureText: isSecret,
+      onSubmitted: ((newValue){
+       // print(newValue);
+      updateUser();
+      }),
+    );
+  }
+
+  updateUser(){
+    setState(() {
+      myProfile = Profile(
+        surname: (surname.text != myProfile.surname) ? surname.text : myProfile.surname,
+        name: (name.text != myProfile.name) ? name.text : myProfile.name,
+        secret:(secret.text != myProfile.secret) ? secret.text : myProfile.secret,
+      );
+    });
   }
 
 }
---
+++ lib/profile.dart
@@ -0,0 +1,53 @@
+
+class Profile {
+  String surname;
+  String name;
+  int age;
+  bool gender;
+  double height;
+  String secret;
+  List<String> hobbies;
+  String favoriteLang;
+
+  Profile({
+    this.surname ="",
+    this.name = "",
+    this.age = 0,
+    this.gender = true,
+    this.height = 0.0,
+    this.secret = "",
+    this.hobbies = const [],
+    this.favoriteLang = 'dart',
+});
+
+ // String setName() => "$surname $name";
+
+  String setName(){
+    return "$surname $name";
+  }
+
+  String setAge(){
+    String ageString = "$age an";
+    if (age>1){
+      ageString += "s";
+    }
+    return ageString;
+  }
+
+  String genderString() => (gender) ? 'feminin' : 'Masculin';
+
+  String setHeight() => '$height cm';
+
+  String setHobbies(){
+    String tohobbiesString = '';
+    if (hobbies.isEmpty){
+      return tohobbiesString;
+    }else{
+      tohobbiesString = 'mes hobbies sont :';
+      for (var hobbies in hobbies) {
+        tohobbiesString += '$hobbies,';
+      }
+      return tohobbiesString;
+    }
+  }
+}
